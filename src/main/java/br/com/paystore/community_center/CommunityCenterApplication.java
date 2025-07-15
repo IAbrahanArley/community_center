2e6ebe8403986524da0dd5e67100e8be6ba8f4e1
abrah <abrahanarley96@gmail.com>
2025-07-15 20:10:14 -0300

    Chore: configuracao inicial do pom e conteinerizacao

---
+++ src/main/java/br/com/paystore/community_center/CommunityCenterApplication.java
@@ -0,0 +1,13 @@
+package br.com.paystore.community_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/paystore/community_center/CommunityCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paystore.community_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
