363768896cf02ddaecffe98998c8c9209d7c32c6
abrah <abrahanarley96@gmail.com>
2025-07-16 21:39:55 -0300

    Refactor: http status

--- src/main/java/br/com/paystore/community_center/controllers/NegociacaoController.java
+++ src/main/java/br/com/paystore/community_center/controllers/NegociacaoController.java
@@ -6,6 +6,8 @@ import br.com.paystore.community_center.dto.NegociacaoResponseDTO;
 import br.com.paystore.community_center.services.NegociacaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -19,15 +21,17 @@ public class NegociacaoController {
     private final NegociacaoService service;
 
     @PostMapping
-    public NegociacaoResponseDTO negociar(@RequestBody NegociacaoRequestDTO dto) {
-        return service.negociar(dto);
+    public ResponseEntity<NegociacaoResponseDTO> negociar(@RequestBody NegociacaoRequestDTO dto) {
+        NegociacaoResponseDTO response = service.negociar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public List<NegociacaoResponseDTO> listarPorCentroEData(
+    public ResponseEntity<List<NegociacaoResponseDTO>> listarPorCentroEData(
             @RequestParam String centroId,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime desde
     ) {
-        return service.listarPorCentroEData(centroId, desde);
+        List<NegociacaoResponseDTO> negociacoes = service.listarPorCentroEData(centroId, desde);
+        return ResponseEntity.ok(negociacoes);
     }
 }
